44f978b478a8355b599db6124dbd6cfb3241616f
Sunil Khadka <sunil.khadka001@gmail.com>
2024-09-19 17:22:37 +0545

    T1: Setting up a simple web socket connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,44 @@
+import React, { useEffect, useState } from "react";
 
-function App() {
+// Define the WebSocket type as WebSocket | null because it can be initially null
+const App: React.FC = () => {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [input, setInput] = useState<string>("");
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8000/");
+    setSocket(ws);
+
+    ws.onmessage = (event: MessageEvent) => {
+      setMessages((prev) => [...prev, event.data]);
+    };
+
+    // Cleanup the WebSocket connection when the component unmounts
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socket && input) {
+      socket.send(input);
+      setInput(""); // Clear the input after sending
+    }
+  };
 
   return (
-   <div>
-    <h1>Hello Chat Application</h1>
-   </div>
-  )
-}
+    <div>
+      <h1>Chat</h1>
+      <div>
+        {messages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
